b8519eb879d5319a82fa2d8b383d427fe05ac13d
sara <sara.atef001@gmail.com>
2024-10-03 09:43:35 +0300

    Tours Project Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect } from "react";
+import { useState } from "react";
+
+import Loading from './components/loading'
+import Tours from './components/Tours'
+
+const url = '/api/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [ isLoading, setIsLoading ] = useState( true );
+  const [ tours, setTours ] = useState( [] )
+
+  const removeTour = ( id ) => {
+    setTours( tours.filter( tour => tour.id != id ) )
+  }
+  const fetchTours = async () => {
+    setIsLoading( true )
+    try {
+      const response = await fetch( url )
+      const tours = await response.json();
+      setTours( tours )
+    } catch ( error ) {
+      console.log( error )
+    }
+    setIsLoading( false )
+  }
+  useEffect( () => { fetchTours() }, [] )
+
+  if ( isLoading ) {
+    return <main>
+      <Loading />
+    </main>
+  }
+
+  if ( tours.length === 0 ) {
+    return <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+        <button style={{ marginTop: '2rem' }} className="btn" onClick={fetchTours}>Fetch Tours</button>
+      </div>
+    </main>
+  }
+  return <main>
+    <Tours tours={tours} removeTour={removeTour} />
+  </main>;
 };
 export default App;
---
+++ src/components/Tour.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Tour = ( { id, image, info, name, price, removeTour } ) => {
+    const [ readMore, setReadMore ] = useState( false )
+    return (
+        <article className='single-tour'>
+            <img src={image} alt={name} className='img' />
+            <span className='tour-price'>${price}</span>
+            <div className='tour-info'>
+                <h5>{name}</h5>
+                <p>{readMore ? info : `${info.substring( 0, 200 )}.....`} <button className='info-btn' onClick={() => setReadMore( !readMore )}>{readMore ? "Show Less" : "Read More"}</button></p>
+                <button onClick={() => removeTour( id )} className='btn  btn-block delete-btn'>Not Interested</button>
+            </div>
+        </article>
+    )
+}
+
+export default Tour
